b3a99d901af2af66295babea352e0a5a4eba3197
Adwita Garg <90994199+agarg09@users.noreply.github.com>
2025-11-01 17:40:05 -0700

    Create RepoParser.py (Adwita's code, Vishal enter code here)

---
+++ src/parser/RepoParser.py
@@ -0,0 +1,11 @@
+"""
+Parses through the git commit history for this branch (passed in git commit history)
+"""
+
+
+class RepoParser:
+    def __init__(self, branch: str):
+        self.branch = branch
+
+    def parse(branch: str):
+        return "temporary working state"
