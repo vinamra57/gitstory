59738d0fff547d2564971a21aed3e95d1ff5e694
Adwita Garg <90994199+agarg09@users.noreply.github.com>
2025-11-01 17:27:25 -0700

    Update test_main.py (with new tests + comments)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
-# WARNING: THIS TEST IS ONLY FOR THE MOST RUDIMENTARY VERSION OF main.py
-# JUST TO MAKE SURE MY INITIAL VERSION WORKS
-# ALTER WHEN U ACTUALLY IMPLEMENT IT
 from click.testing import CliRunner
 from main import cli
+
+# Tests the main features of GitStory: run, dashboard, since, and compare
+# Further specific tests for each component are located in their respective folders
+# within the tests folder
 
 
 class TestMain:
@@ -15,22 +16,22 @@ class TestMain:
         runner = CliRunner()
         result = runner.invoke(cli, ["run"])
         assert result.exit_code == 0
-        assert "RUN TO BE COMPLETED" in result.output
-
-    def test_main_compare(self):
-        runner = CliRunner()
-        result = runner.invoke(cli, ["compare"])
-        assert result.exit_code == 0
-        assert "COMPARE TO BE COMPLETED" in result.output
+        assert "Summary generation complete!" in result.output
 
     def test_main_dashboard(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["dashboard"])
         assert result.exit_code == 0
-        assert "DASHBOARD TO BE COMPLETED" in result.output
+        assert "Dashboard saved!" in result.output
 
     def test_main_since(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["since"])
         assert result.exit_code == 0
         assert "SINCE TO BE COMPLETED" in result.output
+
+    def test_main_compare(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ["compare"])
+        assert result.exit_code == 0
+        assert "COMPARE TO BE COMPLETED" in result.output
