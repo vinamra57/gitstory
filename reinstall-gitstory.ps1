5cb056c72883545baf1e3d5766f90f9cbe7f1f59
TheHorseProgrammer <45744391+TheHorseProgrammer@users.noreply.github.com>
2025-12-08 15:50:24 -0800

    script alterations to properly clean out the build folders

--- reinstall-gitstory.ps1
+++ reinstall-gitstory.ps1
@@ -1,5 +1,5 @@
 # Windows script built for testing, similar to reinstall-gitstory.sh
-
+Remove-Item -Path ./dist/gitstory-*.tar.gz  -Recurse -Force
 uv build --no-sources --sdist
 pipx uninstall gitstory
 $files = Get-ChildItem ./dist/gitstory-*.tar.gz
