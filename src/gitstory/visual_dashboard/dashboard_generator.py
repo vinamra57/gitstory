4ffd63da9254e010ab76f0eb2539df49402c2f40
syliu04 <shengyaoliu02@gmail.com>
2025-12-07 22:18:07 -0800

    UI Update
    - added visualizations
    - major UI changes

--- src/gitstory/visual_dashboard/dashboard_generator.py
+++ src/gitstory/visual_dashboard/dashboard_generator.py
@@ -13,10 +13,13 @@ TEMPLATE_DIR = os.path.join(CURR_DIR, "templates")
 # We tell it to look for templates inside the folder specific in the TEMPLATE_DIR path
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
-def generate_dashboard(repo_data: dict, ai_summary: dict, repo_path: str, output_file="dashboard.html"):
+
+def generate_dashboard(
+    repo_data: dict, ai_summary: dict, repo_path: str, output_file="dashboard.html"
+):
     # Here dict is basically to specify that it should be a JSON
     # These take the current path to this folder, and appends "output"
-    
+
     OUTPUT_DIR = os.path.join(repo_path, "output")
     # This goes to the OUTPUT_DIR path and adds an output folder if it doesnt exist yet
     os.makedirs(OUTPUT_DIR, exist_ok=True)
@@ -39,6 +42,10 @@ def generate_dashboard(repo_data: dict, ai_summary: dict, repo_path: str, output
     css_source = os.path.join(CURR_DIR, "static", "styles.css")  # adjust if needed
     css_dest = os.path.join(OUTPUT_DIR, "styles.css")
     shutil.copyfile(css_source, css_dest)
+
+    js_source = os.path.join(CURR_DIR, "static", "script.js")
+    js_dest = os.path.join(OUTPUT_DIR, "script.js")
+    shutil.copyfile(js_source, js_dest)
 
     # We append the output file to the OUTPUT_DIR path
     output_path = os.path.join(OUTPUT_DIR, output_file)
